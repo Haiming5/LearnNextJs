8ab59963ed35db6066231bad92d1423104d044a0
Glen Wu <glenwu1998@gmail.com>
2024-05-23 19:48:51 -0400

    added pagination

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,14 +1,22 @@
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import {
+  PencilIcon,
+  PlusIcon,
+  TrashIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
+// import { useSearchParams } from 'next/navigation';
 
 export function CreateInvoice() {
+  // const searchParams = useSearchParams();
+  // console.log(searchParams.get('query'));
   return (
     <Link
       href="/dashboard/invoices/create"
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
       <span className="hidden md:block">Create Invoice</span>{' '}
-      <PlusIcon className="h-5 md:ml-4" />
+      <PlusCircleIcon className="h-5 md:ml-4" />
     </Link>
   );
 }
